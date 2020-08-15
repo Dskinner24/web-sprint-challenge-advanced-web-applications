65a3b3d0cd8efd05eada25dde15d359a873594ad
unknown <dskinner_93@yahoo.com>
2020-08-14 19:07:33 -0700

    colors/bubbles not rendering to screen

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
+  const [login, setLogin] = useState({
+    username: '',
+    password: ''
+  })
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const handleChange = (e) => {
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value
+    })
+    console.log(login)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/login', login)
+      .then(res => {
+        console.log(res);
+        window.localStorage.setItem('token', res.data.payload)
+      })
+    props.history.push('/colors')
+  }
+
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='username'
+          label='username'
+          placeholder='Username'
+          value={login.username}
+          onChange={handleChange}
+          className='input'
+        ></input>
+        <input
+          type='password'
+          name='password'
+          label='password'
+          placeholder='Password'
+          value={login.password}
+          onChange={handleChange}
+          className='input'
+        ></input>
+        <button>LogIn</button>
+      </form>
+      
+    </div>
   );
 };
 
